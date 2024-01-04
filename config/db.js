3c8938e083f4367aed285dbee2bba6b3c179f871
shashi <shashikumarsharma59@gmail.com>
2024-01-04 18:21:56 +0530

    cluster

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://localhost:27017/practice'
+const url = 'mongodb+srv://shashikumarsharma59:shashi00$@cluster0.ajezqkh.mongodb.net/'
 mongoose.connect(url).then((res) => console.log( "mongoose connect")).catch((error) => console.log(error))
 
