ad8b8ec1bba510a527f5c28f1002788f0492ccd6
shashi <shashikumarsharma59@gmail.com>
2024-01-05 14:25:37 +0530

    atlas add

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
-const url = 'mongodb+srv://shashikumarsharma59:shashi00$@cluster0.ajezqkh.mongodb.net/'
+const url = 'mongodb+srv://shashikumarsharma59:shashi@cluster0.ajezqkh.mongodb.net/dashboard?retryWrites=true&w=majority'
 mongoose.connect(url).then((res) => console.log( "mongoose connect")).catch((error) => console.log(error))
 
